999aae68b1982f4d49c4c11145ff86d21c59e29e
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-21 17:29:40 +0530

    FLUT-3560 - Sample and screenshot added.

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() => runApp(TappedAppointment());
+
+class TappedAppointment extends StatefulWidget {
+  @override
+  CalendarAppointment createState() => CalendarAppointment();
+}
+
+class CalendarAppointment extends State<TappedAppointment> {
+  CalendarDataSource _dataSource;
+
+  @override
+  void initState() {
+    _dataSource = _getDataSource();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: SfCalendar(
+            dataSource: _dataSource,
+            onTap: calendarTapped,
+            allowedViews: [
+              CalendarView.day,
+              CalendarView.week,
+              CalendarView.workWeek,
+              CalendarView.month,
+              CalendarView.timelineDay,
+              CalendarView.timelineWeek,
+              CalendarView.timelineWorkWeek,
+              CalendarView.timelineMonth,
+              CalendarView.schedule
+            ],
+            monthViewSettings: MonthViewSettings(showAgenda: true),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void calendarTapped(CalendarTapDetails calendarTapDetails) {
+    Appointment app = Appointment(
+        startTime: calendarTapDetails.date,
+        endTime: calendarTapDetails.date.add(Duration(hours: 1)),
+        subject: 'Tapped appointment',
+        color: Colors.greenAccent);
+    _dataSource.appointments.add(app);
+    _dataSource.notifyListeners(
+        CalendarDataSourceAction.add, _dataSource.appointments);
+  }
+
+  _DataSource _getDataSource() {
+    List<Appointment> appointments = <Appointment>[];
+    appointments.add(Appointment(
+      startTime: DateTime.now(),
+      endTime: DateTime.now().add(Duration(hours: 1)),
+      subject: 'Meeting',
+      color: Colors.teal,
+    ));
+    return _DataSource(appointments);
+  }
+}
+
+class _DataSource extends CalendarDataSource {
+  _DataSource(List<Appointment> source) {
+    appointments = source;
+  }
+}
